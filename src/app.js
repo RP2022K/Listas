8c6e7dd9f0ea315febf3e6c326958c006245c27b
RP <RP@mail.com>
2023-01-24 20:16:23 +0100

    eseménykezelés hozzáadva

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const nevLista = document.querySelector("#nevLista");
+const nameElem = document.querySelector("#name");
+const addButton = document.querySelector("#addButton");
+
 var nevTomb = ["Pista", "Mari", "Kari", "Joli", "Pali", "Gabi", "Józsi", "Teri"];
 
 nevTomb.forEach((nev) => {
@@ -8,5 +11,17 @@ nevTomb.forEach((nev) => {
     nevLista.appendChild(li);
 });
 
+addButton.addEventListener('click', () =>{
+    nevTomb.push(nameElem.value);
+    //console.log(nevTomb);
 
+    addLiElem(nameElem.value);
+    nameElem.value = '';
+});
 
+function addLiElem(name){
+    var li = document.createElement("li");
+    li.textContent = name;
+    li.className = 'list-group-item';
+    nevLista.appendChild(li);
+}
